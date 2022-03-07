54a7c9075da6e9721b444d4ef5922493ac4c99a9
HishamAfifi <hisham.mhammed.afifi@gmail.com>
2022-03-07 20:18:56 +0200

    remove warnings

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
-import { useCallback } from "react/cjs/react.production.min";
 
 function App() {
   const { authIsReady, user } = useAuthContext();
